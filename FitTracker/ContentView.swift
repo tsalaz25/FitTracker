da9c82a4fb3445595c30e9fa170c97736fae8c86
Tomas <tomas.salaz.25@gmail.com>
2026-08-25 21:46:49 -0600

    Initial Commit

---
+++ FitTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitTracker
+//
+//  Created by Tomas Salaz on 8/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitTracker/FitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitTrackerApp.swift
+//  FitTracker
+//
+//  Created by Tomas Salaz on 8/25/26.
+//
+
+import SwiftUI
+
+@main
+struct FitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
